6ed0e4df004854757b7cc6a130bbb1d96385c162
ACTC <s12327275@stu.najah.edu>
2026-07-13 19:40:18 +0300

    Add DateService and Mockito unit test

---
+++ src/test/java/com/vehiclerental/tests/DateServiceTest.java
@@ -0,0 +1,28 @@
+package com.vehiclerental.tests;
+
+import com.vehiclerental.service.DateService;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class DateServiceTest {
+
+    @Test
+    void testCurrentDateMock() {
+
+        DateService dateService = mock(DateService.class);
+
+        when(dateService.getCurrentDate())
+                .thenReturn(LocalDate.of(2025, 7, 1));
+
+        assertEquals(
+                LocalDate.of(2025, 7, 1),
+                dateService.getCurrentDate()
+        );
+
+    }
+
+}
